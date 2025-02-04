83ce71d40b32582147e1278f2bb87bdb8bc1096c
JorgeSaicoski <jorgesaicoski@gmail.com>
2025-02-04 19:08:41 -0300

    auto migrate subscriptions

--- config/database.go
+++ config/database.go
@@ -15,6 +15,6 @@ func ConnectDatabase() *gorm.DB {
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
-	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.User{}, &models.Subscription{})
 	return db
 }
